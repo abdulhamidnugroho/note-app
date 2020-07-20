ea6a60ab07beb02636dcc276dc55a0241a34aee1
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-07-20 10:50:05 +0700

    Listing Notes

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ yargs.command({
     command: 'list',
     describe: 'List notes',
     handler () {
-        console.log('list from note')
+        notes.listNotes()
     }
 })
 
--- notes.js
+++ notes.js
@@ -48,9 +48,20 @@ const removeNote = (title) => {
 
     saveNotes(notesToKeep)
 }
+
+const listNotes = () => {
+    const notes = loadNotes()
+
+    console.log(chalk.inverse('Your Notes'))
+
+    notes.forEach(note => {
+       console.log(note.title)
+    })
+}
  
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
-    removeNote: removeNote
+    removeNote: removeNote,
+    listNotes: listNotes
 }
